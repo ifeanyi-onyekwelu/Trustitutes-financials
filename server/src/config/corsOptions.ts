0c08b62a3e995ee4c18429af3b8f13fdde4842e3
ifekel <ifeanyionyekwelu786@gmail.com>
2024-10-31 20:24:29 +0100

    Cors

--- server/src/config/corsOptions.ts
+++ server/src/config/corsOptions.ts
@@ -1,4 +1,8 @@
-const allowedOrigins = ["http://localhost:5173", "http://127.0.0.1:5173"];
+const allowedOrigins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://www.trustitutesfinancials.com",
+];
 
 const corsOptions = {
     origin: (
